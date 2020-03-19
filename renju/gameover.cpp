543f484dd89a405bb182cc2ceebb65d126758ebd
fdy17 <52491470+fdy17@users.noreply.github.com>
2020-03-19 15:25:42 +0800

    init version

---
+++ renju/gameover.cpp
@@ -0,0 +1,5 @@
+#include "define.h"
+bool gameover(struct point node, int player)//ÅĞ¶ÏÊÇ·ñÓÎÏ·½áÊø
+{
+	return true;
+}
---
+++ renju/start.cpp
@@ -0,0 +1,14 @@
+#include "define.h"
+#include "searchmove.h"
+#include "printchessboard.h"
+#include "makemove.h"
+#include "gameover.h"
+int main()
+{
+	cout << "*************五子棋人机对弈AI*************" << endl;
+	cout << "输入: newblack  电脑先手" << endl;
+	cout << "输入: newwhite  电脑后手" << endl;
+	cout << "输入: move x y  表示落子点" << endl;
+    print();
+	cout << gameover() << endl;
+}
